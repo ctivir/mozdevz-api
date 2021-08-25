d71b4388557e68f7e8cedfd14d75b9734593d9e2
ctivir <ctivir@gmail.com>
2021-08-25 14:52:29 +0200

    Adding hosts

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7qt0^9-htzgahnjqc955))(zi@qs_%#z=jol&$ckh3kj*r=di0'
+#SECRET_KEY = '7qt0^9-htzgahnjqc955))(zi@qs_%#z=jol&$ckh3kj*r=di0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['mzdevz-api.herokuapp.com']
 
