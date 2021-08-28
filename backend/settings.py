094286bc44f33adf733182c74e4b76012ba297df
ctivir <ctivir@gmail.com>
2021-08-28 09:08:27 +0200

    Config db

--- backend/settings.py
+++ backend/settings.py
@@ -85,8 +85,8 @@ TEMPLATES = [
 ]
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
+    'https://localhost:3000',
+    'https://127.0.0.1:3000',
 ]
 
 WSGI_APPLICATION = 'backend.wsgi.application'
